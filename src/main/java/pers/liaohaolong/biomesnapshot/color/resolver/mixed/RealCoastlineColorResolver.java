e97a89f74597a0d027926bcb36b478617611d7c5
廖浩龙 <aliaohaolong@qq.com>
2025-08-02 22:31:50 +0800

    迁移到1.18

--- src/main/java/pers/liaohaolong/biomesnapshot/color/resolver/mixed/RealCoastlineColorResolver.java
+++ src/main/java/pers/liaohaolong/biomesnapshot/color/resolver/mixed/RealCoastlineColorResolver.java
@@ -22,7 +22,7 @@ public class RealCoastlineColorResolver implements ColorResolver {
         Chunk chunk = world.getChunkManager().getChunk(
                 ChunkSectionPos.getSectionCoord(x),
                 ChunkSectionPos.getSectionCoord(z),
-                ChunkStatus.CARVERS,
+                ChunkStatus.NOISE,
                 true
         );
 
