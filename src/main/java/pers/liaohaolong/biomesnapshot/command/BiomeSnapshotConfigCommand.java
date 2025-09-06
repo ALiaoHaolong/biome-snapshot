5a30624a8244231f3139ce58a7dd82ec2feda179
廖浩龙 <aliaohaolong@qq.com>
2025-09-06 19:56:03 +0800

    将配置命令移动到biome-snapshot-config命令下； 新增支持配置大陆海洋生物群系颜色解析器和大陆河流海洋生物群系颜色解析器； 完善命令参数提示（namespace、path和color）； 规格化输出Hex颜色代码（不足6位的前补0）； 配置命令的反馈中增加所配置的颜色解析器ID作为反馈前缀

--- src/main/java/pers/liaohaolong/biomesnapshot/command/BiomeSnapshotConfigCommand.java
+++ src/main/java/pers/liaohaolong/biomesnapshot/command/BiomeSnapshotConfigCommand.java
@@ -6,55 +6,147 @@ import com.mojang.brigadier.context.CommandContext;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
+import pers.liaohaolong.biomesnapshot.BiomeSnapshotUtils;
 import pers.liaohaolong.biomesnapshot.color.resolver.ColorResolvers;
 import pers.liaohaolong.biomesnapshot.color.resolver.biome.BiomeColorResolver;
+import pers.liaohaolong.biomesnapshot.color.resolver.biome.MainlandOceanBiomeColorResolver;
+import pers.liaohaolong.biomesnapshot.color.resolver.biome.MainlandRiverOceanBiomeColorResolver;
 
+/**
+ * <h3>/biome-snapshot-config 命令</h3>
+ */
 public class BiomeSnapshotConfigCommand {
 
-    public int set(CommandContext<ServerCommandSource> context) {
-        // 读参
-        String namespace = StringArgumentType.getString(context, "namespace");
-        String path = StringArgumentType.getString(context, "path");
-        int color = IntegerArgumentType.getInteger(context, "color");
-        // 标识符
-        Identifier identifier = Identifier.of(namespace, path);
-        // 业务
-        BiomeColorResolver biomeColorResolver = (BiomeColorResolver) ColorResolvers.BIOME_COLOR_RESOLVER;
-        biomeColorResolver.getBiomeColorMap().put(identifier, color);
-        // 反馈
-        context.getSource().sendFeedback(() ->
-                Text.translatable("command.biome-snapshot.config.biome_color_resolver.set.success", identifier.toString(), String.valueOf(color), Integer.toHexString(color).toUpperCase()), true);
-        return 1;
+    protected void sendFeedback(CommandContext<ServerCommandSource> context, String key, Object... args) {
+        context.getSource().sendFeedback(() -> Text.translatable(key, args), true);
     }
 
-    public int remove(CommandContext<ServerCommandSource> context) {
-        // 读参
-        String namespace = StringArgumentType.getString(context, "namespace");
-        String path = StringArgumentType.getString(context, "path");
-        // 标识符
-        Identifier identifier = Identifier.of(namespace, path);
-        // 业务
-        BiomeColorResolver biomeColorResolver = (BiomeColorResolver) ColorResolvers.BIOME_COLOR_RESOLVER;
-        biomeColorResolver.getBiomeColorMap().remove(identifier);
-        // 反馈
-        context.getSource().sendFeedback(() ->
-                Text.translatable("command.biome-snapshot.config.biome_color_resolver.remove.success", identifier.toString()), true);
-        return 1;
+    /**
+     * <h3>/biome-snapshot-config BIOME 命令</h3>
+     */
+    public static class BiomeCommand extends BiomeSnapshotConfigCommand {
+
+        private final BiomeColorResolver colorResolver = (BiomeColorResolver) ColorResolvers.BIOME_COLOR_RESOLVER;
+
+        public int set(CommandContext<ServerCommandSource> context) {
+            String namespace = StringArgumentType.getString(context, "namespace");
+            String path = StringArgumentType.getString(context, "path");
+            int color = IntegerArgumentType.getInteger(context, "color");
+
+            Identifier identifier = Identifier.of(namespace, path);
+            colorResolver.getBiomeColorMap().put(identifier, color);
+            sendFeedback(context, "command.biome-snapshot.config.biome_color_resolver.set.success", identifier.toString(), String.valueOf(color), BiomeSnapshotUtils.toHexString(color));
+            return 1;
+        }
+
+        public int remove(CommandContext<ServerCommandSource> context) {
+            String namespace = StringArgumentType.getString(context, "namespace");
+            String path = StringArgumentType.getString(context, "path");
+
+            Identifier identifier = Identifier.of(namespace, path);
+            colorResolver.getBiomeColorMap().remove(identifier);
+            sendFeedback(context, "command.biome-snapshot.config.biome_color_resolver.remove.success", identifier.toString());
+            return 1;
+        }
+
+        public int get(CommandContext<ServerCommandSource> context) {
+            String namespace = StringArgumentType.getString(context, "namespace");
+            String path = StringArgumentType.getString(context, "path");
+
+            Identifier identifier = Identifier.of(namespace, path);
+            int color = colorResolver.getBiomeColorMap().get(identifier);
+            sendFeedback(context, "command.biome-snapshot.config.biome_color_resolver.get", identifier.toString(), String.valueOf(color), BiomeSnapshotUtils.toHexString(color));
+            return 1;
+        }
+
     }
 
-    public int get(CommandContext<ServerCommandSource> context) {
-        // 读参
-        String namespace = StringArgumentType.getString(context, "namespace");
-        String path = StringArgumentType.getString(context, "path");
-        // 标识符
-        Identifier identifier = Identifier.of(namespace, path);
-        // 业务
-        BiomeColorResolver biomeColorResolver = (BiomeColorResolver) ColorResolvers.BIOME_COLOR_RESOLVER;
-        int color = biomeColorResolver.getBiomeColorMap().get(identifier);
-        // 反馈
-        context.getSource().sendFeedback(() ->
-                Text.translatable("command.biome-snapshot.config.biome_color_resolver.get", identifier.toString(), String.valueOf(color), Integer.toHexString(color).toUpperCase()), true);
-        return 1;
+    /**
+     * <h3>/biome-snapshot-config MAINLAND_OCEAN_BIOME 命令</h3>
+     */
+    public static class MainlandOceanBiomeCommand extends BiomeSnapshotConfigCommand {
+
+        private final MainlandOceanBiomeColorResolver colorResolver = (MainlandOceanBiomeColorResolver) ColorResolvers.MAINLAND_OCEAN_BIOME_COLOR_RESOLVER;
+
+        public int setMainland(CommandContext<ServerCommandSource> context) {
+            int color = IntegerArgumentType.getInteger(context, "color");
+
+            colorResolver.setMainlandColor(color);
+            sendFeedback(context, "command.biome-snapshot.config.mainland_ocean_biome_color_resolver.set.mainland", String.valueOf(color), BiomeSnapshotUtils.toHexString(color));
+            return 1;
+        }
+
+        public int setOcean(CommandContext<ServerCommandSource> context) {
+            int color = IntegerArgumentType.getInteger(context, "color");
+
+            colorResolver.setOceanColor(color);
+            sendFeedback(context, "command.biome-snapshot.config.mainland_ocean_biome_color_resolver.set.ocean", String.valueOf(color), BiomeSnapshotUtils.toHexString(color));
+            return 1;
+        }
+
+        public int getMainland(CommandContext<ServerCommandSource> context) {
+            int color = colorResolver.getMainlandColor();
+            sendFeedback(context, "command.biome-snapshot.config.mainland_ocean_biome_color_resolver.get.mainland", String.valueOf(color), BiomeSnapshotUtils.toHexString(color));
+            return 1;
+        }
+
+        public int getOcean(CommandContext<ServerCommandSource> context) {
+            int color = colorResolver.getOceanColor();
+            sendFeedback(context, "command.biome-snapshot.config.mainland_ocean_biome_color_resolver.get.ocean", String.valueOf(color), BiomeSnapshotUtils.toHexString(color));
+            return 1;
+        }
+
+    }
+
+    /**
+     * <h3>/biome-snapshot-config MAINLAND_RIVER_OCEAN_BIOME 命令</h3>
+     */
+    public static class MainlandRiverOceanBiomeCommand extends BiomeSnapshotConfigCommand {
+
+        private final MainlandRiverOceanBiomeColorResolver colorResolver = (MainlandRiverOceanBiomeColorResolver) ColorResolvers.MAINLAND_RIVER_OCEAN_BIOME_COLOR_RESOLVER;
+
+        public int setMainland(CommandContext<ServerCommandSource> context) {
+            int color = IntegerArgumentType.getInteger(context, "color");
+
+            colorResolver.setMainlandColor(color);
+            sendFeedback(context, "command.biome-snapshot.config.mainland_river_ocean_biome_color_resolver.set.mainland", String.valueOf(color), BiomeSnapshotUtils.toHexString(color));
+            return 1;
+        }
+
+        public int setRiver(CommandContext<ServerCommandSource> context) {
+            int color = IntegerArgumentType.getInteger(context, "color");
+
+            colorResolver.setRiverColor(color);
+            sendFeedback(context, "command.biome-snapshot.config.mainland_river_ocean_biome_color_resolver.set.river", String.valueOf(color), BiomeSnapshotUtils.toHexString(color));
+            return 1;
+        }
+
+        public int setOcean(CommandContext<ServerCommandSource> context) {
+            int color = IntegerArgumentType.getInteger(context, "color");
+
+            colorResolver.setOceanColor(color);
+            sendFeedback(context, "command.biome-snapshot.config.mainland_river_ocean_biome_color_resolver.set.ocean", String.valueOf(color), BiomeSnapshotUtils.toHexString(color));
+            return 1;
+        }
+
+        public int getMainland(CommandContext<ServerCommandSource> context) {
+            int color = colorResolver.getMainlandColor();
+            sendFeedback(context, "command.biome-snapshot.config.mainland_river_ocean_biome_color_resolver.get.mainland", String.valueOf(color), BiomeSnapshotUtils.toHexString(color));
+            return 1;
+        }
+
+        public int getRiver(CommandContext<ServerCommandSource> context) {
+            int color = colorResolver.getRiverColor();
+            sendFeedback(context, "command.biome-snapshot.config.mainland_river_ocean_biome_color_resolver.get.river", String.valueOf(color), BiomeSnapshotUtils.toHexString(color));
+            return 1;
+        }
+
+        public int getOcean(CommandContext<ServerCommandSource> context) {
+            int color = colorResolver.getOceanColor();
+            sendFeedback(context, "command.biome-snapshot.config.mainland_river_ocean_biome_color_resolver.get.ocean", String.valueOf(color), BiomeSnapshotUtils.toHexString(color));
+            return 1;
+        }
+
     }
 
 }
